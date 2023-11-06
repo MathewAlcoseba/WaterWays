9c373f63be056132074b9dfed81cd153813d117c
Czar Basanal <czarbasanal@gmail.com>
2023-11-06 17:35:25 +0800

    ilong

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WaterWays',
+      title: 'WaterWays Ilong',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: const Color(0xffF8F8F8),

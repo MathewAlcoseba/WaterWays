e81825e60c99ad7646958d40a311bc9af3291daa
Czar Basanal <czarbasanal@gmail.com>
2023-11-15 00:11:06 +0800

    edited bottom navbar

--- lib/bottom-navbar.dart
+++ lib/bottom-navbar.dart
@@ -39,6 +39,8 @@ class BottomNavBar extends StatelessWidget {
       buttonBackgroundColor: AppStyles.colorScheme.primary,
       index: selectedIndex,
       items: navBarIcons,
+      animationCurve: Curves.easeInOut,
+      animationDuration: const Duration(milliseconds: 400),
       onTap: onItemTapped,
     );
   }
